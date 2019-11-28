bdc81077858bb934e748af1414f1fc2068716ddf
Ron Crisostomo <rcrisostomo97@gmail.com>
2019-11-28 14:54:16 +1300

    Initial Commit

---
+++ Tasker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tasker
+//
+//  Created by Ron Crisostomo on 28/11/19.
+//  Copyright © 2019 Ron Crisostomo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
